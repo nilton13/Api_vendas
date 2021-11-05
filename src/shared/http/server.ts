7ca769e83df1dd1bfc9bc0c52c2b5ae3af3a406e
Nilton F <jnvlfilho@hotmail.com>
2021-11-04 23:23:44 -0300

    Criando estrutura e Middleware para tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes'
+import AppError from '../errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+//Middleware para tratamento de erros
+app.use((error: Error, request: Request, response: Response, next: NextFunction) =>{
+    if (error instanceof AppError){ // Verifica se o erro é uma intância da classe AppError.
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'    
+    })
+})
 
 app.listen(8080, () =>{
     console.log("Server started on PORT 8080!")
