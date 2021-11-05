32d585c2a57177c7ccaf245c0c43d31f2db15cd8
Nilton F <jnvlfilho@hotmail.com>
2021-11-04 23:12:36 -0300

    Criando pastas e primeira rota

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello Dev!")
+import express from 'express';
+import cors from 'cors';
+import routes from './routes'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(8080, () =>{
+    console.log("Server started on PORT 8080!")
+})
