dfc7a4d0fa54a739d68b5a8bdc20b073b48bda54
Nilton F <jnvlfilho@hotmail.com>
2021-11-07 00:39:04 -0300

    Utilizando a biblitoteca JSONWEBTOKEN para criaçao do token de autenticacao.

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,4 +1,5 @@
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { getCustomRepository } from "typeorm";
 import AppError from "../../../shared/errors/AppError";
 import User from "../typeorm/entities/User";
@@ -7,11 +8,16 @@ import UsersRepository from "../typeorm/repositories/UsersRepository";
 interface IRequest {
     email: string;
     password: string;  
-}   
+}  
+
+interface IResponse{
+    user: User,
+    token: string
+}
 
 //Serviço para criação de um novo Usuário.
 class CreateSessionsService {
-    public async execute({email,password}: IRequest): Promise<User> {
+    public async execute({email,password}: IRequest): Promise<IResponse> {
         const usersRepository = getCustomRepository(UsersRepository);
         const user = await usersRepository.findByEmail(email);
 
@@ -26,7 +32,15 @@ class CreateSessionsService {
             throw new AppError('Incorrect email/password combination.', 401);
         }
 
-        return user;
+        const token = sign({}, '51f9c8058d1e3760e590d686509224b3', {
+            subject: user.id,
+            expiresIn: '1d', // Tempo de duração. 
+        })
+
+        return {
+            user,
+            token,
+        };
     }
 }
 
