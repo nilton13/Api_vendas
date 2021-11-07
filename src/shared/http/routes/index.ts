ea487ed50cec5a99acc55399cea0b0e24348c1ae
Nilton F <jnvlfilho@hotmail.com>
2021-11-07 18:18:17 -0300

    Implementando açoes de ediçao e exibiçao de Perfil.

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import productsRouter from "../../../modules/products/routes/products.routes";
 import passwordRouter from "../../../modules/users/routes/password.routes";
+import profileRouter from "../../../modules/users/routes/profile.routes";
 import sessionsRouter from "../../../modules/users/routes/sessions.routes";
 import usersRouter from "../../../modules/users/routes/users.routes";
 
@@ -9,7 +10,8 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/password', passwordRouter)
+routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter)
 
 routes.get('/', (request, response) =>{
     return response.json({ message:'Hello Dev!' });
