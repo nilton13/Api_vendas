348bb42a3adad9055f45d6dfbcd3e94648f5a23b
Nilton F <jnvlfilho@hotmail.com>
2021-11-06 23:50:06 -0300

    Criando Servicos Controller e Rotas para inicio de Seçao.

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,33 @@
+import { compare } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import AppError from "../../../shared/errors/AppError";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;  
+}   
+
+//Serviço para criação de um novo Usuário.
+class CreateSessionsService {
+    public async execute({email,password}: IRequest): Promise<User> {
+        const usersRepository = getCustomRepository(UsersRepository);
+        const user = await usersRepository.findByEmail(email);
+
+        //Verificando se o email informado já pertence a um Usuário
+        if(!user){
+            throw new AppError('Incorrect email/password combination.', 401);
+        }
+
+        const passwordConfirmed = await compare(password,user.password);
+
+        if(!passwordConfirmed){
+            throw new AppError('Incorrect email/password combination.', 401);
+        }
+
+        return user;
+    }
+}
+
+export default CreateSessionsService;
