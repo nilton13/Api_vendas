558950ab816f65235f35ed6e0bccf07bde681a42
Nilton F <jnvlfilho@hotmail.com>
2021-11-14 15:42:59 -0300

    Criando teste para criaçao de Customer.

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,10 +1,23 @@
+import 'reflect-metadata';
+import CreateCustomerService from "./CreateCustomerService";
+import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
+
 describe('Create Customer', () => {
     // Espera criar um novo cliente
-    it('should be able to create a new customer', () => {
-        expect(1).toBe(1)
+    it('should be able to create a new customer', async () => {
+        const fakeCustomersRepository = new FakeCustomersRepository();
+
+        const createCustomerService = new CreateCustomerService(fakeCustomersRepository);
+
+        const customer = await createCustomerService.execute({
+            name: 'Nilton',
+            email: 'teste@email.com',
+        });
+
+        expect(customer).toHaveProperty('id');
     });
 
-    // Não posso criar dois clientes com o mesmo email.
+    // Não posso criar dois clientes com o mesmo
     it('should not be able to create two customers with the same email', () => {
         expect(1).toBe(1)
     });
