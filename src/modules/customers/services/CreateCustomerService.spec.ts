09349981cb10e2a82317d79ef0eb1fcf9612f967
Nilton F <jnvlfilho@hotmail.com>
2021-11-14 15:34:31 -0300

    Criando repositorio fake para testes.

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,11 @@
+describe('Create Customer', () => {
+    // Espera criar um novo cliente
+    it('should be able to create a new customer', () => {
+        expect(1).toBe(1)
+    });
+
+    // Não posso criar dois clientes com o mesmo email.
+    it('should not be able to create two customers with the same email', () => {
+        expect(1).toBe(1)
+    });
+});
